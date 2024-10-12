35c26794708e334e0b804f213510a4595c9839e4
Somanath Singh <somanathsingh0803@gmail.com>
2024-10-12 18:22:39 +0530

    Removed Profile Context from mennu and product views

--- Menu/views.py
+++ Menu/views.py
@@ -4,31 +4,24 @@ from Accounts.models import Profile
 # Create your views here.
 
 
-def menu(request,category=None):
-    user=request.user
-    profile=Profile.objects.get(user=user)
-    
+def menu(request,category=None):    
     categories = Category.objects.all()
     if category:
         menu_items = MenuItems.objects.filter(category__name=category)
     else:
         menu_items = MenuItems.objects.all()
-
     context = {
         'menu_items': menu_items,
         'categories': categories,
         'category': category,
-        'profile': profile,
+
     }
     return render(request,'Menu_.html',context)
 
 
 def product(request,pk):
-    user=request.user
-    profile=Profile.objects.get(user=user)
     product=MenuItems.objects.get(id=pk)
     context={
-        'profile':profile,
         'product':product,
     }
     return render(request,'product.html',context)
